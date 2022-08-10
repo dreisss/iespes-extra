b0a65ac0c8aef1f4d91f0867e8f89bb0bd259d9a
Davi Reis <reis.davi11010@gmail.com>
2022-08-10 16:57:53 -0300

    refactor: refactored windows setup script

---
+++ other/scripts/windows/setup.ps1
@@ -0,0 +1,46 @@
+#Requires -RunAsAdministrator
+
+function formatNumber( [string]$number ) {
+  if ($number.Length -lt 2) { return "0$number" }
+  return $number
+}
+
+function setNetworkConfigs() {
+  $addressIPV4    = "192.168.$($primitives.labinNumber).$([int]$primitives.computerNumber + 1)"
+  $defaultGateway = "192.168.$($primitives.labinNumber).1"
+  $primaryDNS     = "8.8.8.8"
+  $secondaryDNS   = "8.8.4.4"
+
+  Write-Host "Changing network configs..." -ForegroundColor Blue
+  New-NetIPAddress -InterfaceAlias "Ethernet" -AddressFamily "ipv4" -IPAddress $addressIPV4 -PrefixLength 24 -DefaultGateway $defaultGateway | Out-Null
+  Set-DnsClientServerAddress -InterfaceAlias "Ethernet" -ServerAddresses ($primaryDNS, $secondaryDNS) | Out-Null
+}
+
+function setComputerName() {
+  $computerName = "LABIN$(formatNumber($primitives.labinNumber))-PC$(formatNumber($primitives.computerNumber))"
+
+  Write-Host "Renaming computer..." -ForegroundColor Blue
+  Rename-Computer -NewName $computerName | Out-Null
+}
+
+function createDefaultUser() {
+  Write-Host "Creating default user..." -ForegroundColor Blue
+  New-LocalUser -Name "Aluno" -NoPassword -UserMayNotChangePassword -AccountNeverExpires | Out-Null
+  Add-LocalGroupMember -SID "S-1-5-32-545" -Member "Aluno" | Out-Null
+}
+
+function runFunctions() {
+  setNetworkConfigs()
+  setComputerName()
+  createDefaultUser()
+}
+
+$primitives = @{
+  labinNumber    = Read-Host "Labin number"
+  computerNumber = Read-Host "Computer number"
+}
+
+Write-Host "Press any key to continue." -ForegroundColor Blue
+[Console]::ReadKey($true) | Out-Null
+
+runFunctions
