7587e5066418315d9bbda2b77892da32cc2ab6fc
Davi Reis <reis.davi11010@gmail.com>
2022-08-17 17:07:37 -0300

    feat(powershell/setup): added avast antivirus to installApps function

--- scripts/powershell/setup.ps1
+++ scripts/powershell/setup.ps1
@@ -96,7 +96,7 @@ function installApps {
   printInfo("Installing apps")
   Start-Sleep -Seconds 30
   [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
-  foreach ($app in @("winrar", "adobereader", "googlechrome", "firefox")) {
+  foreach ($app in @("winrar", "adobereader", "googlechrome", "firefox", "avastfreeantivirus")) {
     Write-Host "Installing $app..." -ForegroundColor Blue
     choco install -y $app | Out-Null
   }
