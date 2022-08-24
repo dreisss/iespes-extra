bf955bfd04e3f3e6ac585687ce6f1f4695eea46c
Davi Reis <reis.davi11010@gmail.com>
2022-08-24 17:01:59 -0300

    feat(powershell/setup): unable tranparency functionality

--- scripts/powershell/setup.ps1
+++ scripts/powershell/setup.ps1
@@ -116,6 +116,7 @@ function styleComputer {
   printInfo("Styling computer")
   Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize -Name SystemUsesLightTheme -Value 0
   Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize -Name AppsUseLightTheme -Value 0
+  Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize -Name EnableTransparency -Value 0
 }
 
 # =====================================================================> Running
