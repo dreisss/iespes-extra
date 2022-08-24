96118a567c6fa47f9312aa740edea0617b501b29
Davi Reis <reis.davi11010@gmail.com>
2022-08-24 17:01:24 -0300

    feat(powershell/setup): change to dark theme functionality

--- scripts/powershell/setup.ps1
+++ scripts/powershell/setup.ps1
@@ -114,6 +114,8 @@ function optimizeComputer {
 
 function styleComputer {
   printInfo("Styling computer")
+  Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize -Name SystemUsesLightTheme -Value 0
+  Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize -Name AppsUseLightTheme -Value 0
 }
 
 # =====================================================================> Running
