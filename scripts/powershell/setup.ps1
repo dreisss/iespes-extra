fab34aa00435e51dc25552d87b80c009c9718835
Davi Reis <reis.davi11010@gmail.com>
2022-09-01 11:05:53 -0300

    feat(powershell/setup): functionality to setup some explorer configurations

--- scripts/powershell/setup.ps1
+++ scripts/powershell/setup.ps1
@@ -182,8 +182,14 @@ function styleComputer {
 function configComputerOther {
   printImportant("Other computer configurations")
   printSecondary("Disabled Bing search on Windows menu")
+  printSecondary("Setting up explorer home page to 'This PC' folder")
+  printSecondary("Disabled showing recent files/folders on 'quick access' in explorer")
+  printSecondary("Disabled showing frequent files/folders on 'quick access' in explorer")
   foreach ($userSID in getUsersSIDList) {
     Set-ItemProperty "Registry::HKEY_USERS\$userSID\SOFTWARE\Microsoft\Windows\CurrentVersion\Search" -Name "BingSearchEnabled" -Type "DWord" -Value 0
+    Set-ItemProperty "Registry::HKEY_USERS\$userSID\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name "LaunchTo" -Type "DWord" -Value 1
+    Set-ItemProperty "Registry::HKEY_USERS\$userSID\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" -Name "ShowRecent" -Type "DWord" -Value 0
+    Set-ItemProperty "Registry::HKEY_USERS\$userSID\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" -Name "ShowFrequent" -Type "DWord" -Value 0
   }
 }
 
