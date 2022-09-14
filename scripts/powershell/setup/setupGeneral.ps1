7b59c5c5a89d16cfc95dd3da850bbf3ce2e55af0
Davi Reis <reis.davi11010@gmail.com>
2022-09-14 15:40:04 -0300

    refactor(powershell/setup/General): organized code and improved cli beauty

--- scripts/powershell/setup/setupGeneral.ps1
+++ scripts/powershell/setup/setupGeneral.ps1
@@ -1,4 +1,9 @@
-param ( [string] $labinNumber, [string] $computerNumber )
+[string] $labinNumber = $args[1]
+[string] $computerNumber = $args[2]
+
+function print( [string] $text) {
+  Write-Host -ForegroundColor "Blue" "   $text"
+}
 
 function formatNumber( [string] $number ) {
   return $(if ($number.Length -lt 2) { "0$number" } else { $number })
@@ -6,35 +11,28 @@ function formatNumber( [string] $number ) {
 
 # ===================================================================> Functions
 function renameComputer {
-  Write-Host " > Renaming Computer... " -BackgroundColor "Blue" -ForegroundColor "Black"
-
   $newName = "LABIN$(formatNumber($labinNumber))-PC$(formatNumber($computerNumber))"
   Rename-Computer -NewName $newName | Out-Null
-
-  Write-Host ""
 }
 
 function createDefaultUser {
-  Write-Host " > Creating Default User... " -BackgroundColor "Blue" -ForegroundColor "Black"
-
   New-LocalUser -Name "Aluno" -NoPassword | Out-Null
   Set-LocalUser -Name "Aluno" -UserMayChangePassword $false  -PasswordNeverExpires $true -AccountNeverExpires | Out-Null
   Add-LocalGroupMember -SID "S-1-5-32-545" -Member "Aluno" | Out-Null
-
-  Write-Host ""
 }
 
 function activateWindows {
-  Write-Host " > Activating Windows... " -BackgroundColor "Blue" -ForegroundColor "Black"
-
   cmd.exe /c slmgr /ipk W269N-WFGWX-YVC9B-4J6C9-T83GX
   cmd.exe /c slmgr /skms kms8.msguides.com
   cmd.exe /c slmgr /ato
-
-  Write-Host ""
 }
 
 # =====================================================================> Running
+print("Renaming computer...")
 renameComputer
+
+print("Creating default user...")
 createDefaultUser
+
+print("Activating Windows...")
 activateWindows
