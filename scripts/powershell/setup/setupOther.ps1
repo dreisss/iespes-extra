701467d49c9f829f75f6e3c1058e7e5ece4edde9
Davi Reis <reis.davi11010@gmail.com>
2022-09-15 14:29:28 -0300

    refactor(powershell/setup/Other): organized and made cli interface prettier

--- scripts/powershell/setup/setupOther.ps1
+++ scripts/powershell/setup/setupOther.ps1
@@ -1,12 +1,26 @@
+function print ( [string] $text ) {
+  Write-Host -ForegroundColor "Blue" "  $text"
+}
+
+# ===================================================================> Functions
 function configComputerOther {
-  foreach ($userSID in getUsersSIDList) {
-    Set-ItemProperty "Registry::HKEY_USERS\$userSID\SOFTWARE\Microsoft\Windows\CurrentVersion\Search" -Name "BingSearchEnabled" -Type "DWord" -Value 0
-    Set-ItemProperty "Registry::HKEY_USERS\$userSID\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name "LaunchTo" -Type "DWord" -Value 1
-    Set-ItemProperty "Registry::HKEY_USERS\$userSID\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" -Name "ShowRecent" -Type "DWord" -Value 0
-    Set-ItemProperty "Registry::HKEY_USERS\$userSID\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" -Name "ShowFrequent" -Type "DWord" -Value 0
-  }
+  # FIXME: disable bing search to all users function
+  print("Disabled bing search on menu")
+
+  # FIXME: Configure explorer launch page function
+  print("Configured explorer launch page to 'This Computer' page")
+
+  # FIXME: Configure 'Fast Access' explorer label - hidden recent function
+  print("Configured 'Fast Access' explorer label to hidden recents")
+
+  # FIXME: Configure 'Fast Access' explorer label - hidden frequent function
+  print("Configured 'Fast Access' explorer label to hidden recents")
+
   powercfg.exe -change -monitor-timeout-ac 0
+  print("Disabled monitor sleeping timeout")
+
   powercfg.exe -change -standby-timeout-ac 0
+  print("Disabled computer sleeping timeout")
 }
 
 configComputerOther
