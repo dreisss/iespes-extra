149ba7ddadfe38441d8829209d4aea6f4bde78fa
Davi Reis <reis.davi11010@gmail.com>
2023-04-03 15:31:09 -0300

    :sparkles: feat(scripts/bash/setup): configure screen timeout and relative function

--- scripts/bash/setup.sh
+++ scripts/bash/setup.sh
@@ -9,4 +9,8 @@ sudo apt install virtualbox virtualbox-ext-pack -y
 wget -O /usr/share/backgrounds/wallpaper.png https://raw.githubusercontent.com/dreisss/iespes-extra/main/design/wallpapers/wallpaper.png
 gsettings set org.gnome.desktop.background picture-uri "/usr/share/backgrounds/wallpaper.png" 
 
+gsettings set org.gnome.desktop.session idle-delay 900
+gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-ac-timeout 3600
+gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-battery-timeout 3600
+
 sudo adduser aluno
