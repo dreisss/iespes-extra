419a93ff9f2e0a2ed222c6d4d9973450856cc1f5
Davi Reis <reis.davi11010@gmail.com>
2023-04-03 14:45:21 -0300

    :sparkles: feat(scripts/bash/setup): function to download and apply wallpaper

--- scripts/bash/setup.sh
+++ scripts/bash/setup.sh
@@ -6,3 +6,6 @@ sudo ifconfig enp2s0 192.168.3.$(($1 + 1))/24
 sudo route add default gw 192.168.3.2
 
 sudo apt install virtualbox virtualbox-ext-pack -y
+
+wget -O /usr/share/backgrounds/wallpaper.png https://raw.githubusercontent.com/dreisss/iespes-extra/main/design/wallpapers/wallpaper.png
+gsettings set org.gnome.desktop.background picture-uri "/usr/share/backgrounds/wallpaper.png" 
