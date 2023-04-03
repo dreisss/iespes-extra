c77b90e77f68f10bf4d85038b6233a40624a7a70
Davi Reis <reis.davi11010@gmail.com>
2023-04-03 15:03:36 -0300

    :bug: fix(scripts/bash/setup): solved problem updating system without network

--- scripts/bash/setup.sh
+++ scripts/bash/setup.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-sudo apt update -y && sudo apt upgrade -y && sudo apt autoremove -y
-
 sudo ifconfig enp2s0 192.168.3.$(($1 + 1))/24
 sudo route add default gw 192.168.3.2
 
+sudo apt update -y && sudo apt upgrade -y && sudo apt autoremove -y
 sudo apt install virtualbox virtualbox-ext-pack -y
 
 wget -O /usr/share/backgrounds/wallpaper.png https://raw.githubusercontent.com/dreisss/iespes-extra/main/design/wallpapers/wallpaper.png
