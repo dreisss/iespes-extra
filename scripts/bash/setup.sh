60c1e8033dbc99e7682858e7aee05c07f4b99d32
Davi Reis <reis.davi11010@gmail.com>
2023-04-05 17:24:49 -0300

    :zap: (scripts/bash/setup): added instructions to configure network and removed useless

--- scripts/bash/setup.sh
+++ scripts/bash/setup.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
+if [ $(nmcli d s | grep " connected" | wc -l) != 1 ]
+then
+  nmcli c m "Wired connection 1" ipv4.addresses "192.168.3.$(($1 + 1))/24"
+  nmcli c m "Wired connection 1" ipv4.gateway "192.168.3.1"
+  nmcli c m "Wired connection 1" ipv4.dns "8.8.8.8"
+  nmcli c m "Wired connection 1" ipv4.method "manual"
+  nmcli c u "Wired connection 1"
+fi
+
 sudo apt update -y && sudo apt upgrade -y && sudo apt autoremove -y
 sudo apt install virtualbox virtualbox-ext-pack -y
 
 wget -O ~/Imagens/wallpaper.png https://raw.githubusercontent.com/dreisss/iespes-extra/main/design/wallpapers/wallpaper.png
 
-gsettings set org.gnome.desktop.session idle-delay 900
-gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-ac-timeout 3600
-gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-battery-timeout 3600
